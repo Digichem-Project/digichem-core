bbe08416f88dedea629a635031107c4056c175f6
oliver-s-lee <osl@st-andrews.ac.uk>
2020-06-28 21:12:20 +0100

    Prep for next version

--- silico/__init__.py
+++ silico/__init__.py
@@ -15,8 +15,8 @@ description = "Silico Computational Chemistry Package"
 development = True
 # Version information.
 major_version = 0
-minor_version = 9
-revision = 10
+minor_version = 10
+revision = 1
 version_number = "{}.{}.{}".format(major_version, minor_version, revision)
 # The full version number of this package.
 version = "{}{}".format(version_number, "-dev" if development else "")
