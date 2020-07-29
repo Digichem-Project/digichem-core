Digichem-Project/digichem-core

commit 56f454adc7aab422686cf4c12b106ec80c80bbfd

Quit with message when the user does not pick any calculations, rather than looping for ever

silico/program/csubmit.py
@@ -143,11 +143,6 @@ def _main(args, config, logger):
 	
 	logger.debug("Loaded {} methods, {} programs and {} calculations".format(len(known_methods), len(known_programs), len(known_calculations)))
 	
-# 	# Print our list if we've been asked to.
-# 	if args.list > 0:
-# 		_list(known_methods, known_programs, known_calculations, all = args.list > 1)
-# 		return 0
-	
 	# Check to see if we've got any files to submit (and prompt if we can).
 	while len(args.calculation_files) == 0 and args.interactive:
 		# Prompt for some files.
@@ -168,8 +163,6 @@ def _main(args, config, logger):
 			calculations = [_parse_calc_string(calc_string, known_methods, known_programs, known_calculations) for calc_string in args.calculations]
 		except:
 			raise Silico_exception("Failed to parse calculations to submit")
-			#logger.error("Failed to parse calculations to submit", exc_info = True)
-			#exit(1)
 						
 		# If a 'dangerous' (one with a warning set) method has been chosen (and we're interactive), get confirmation.
 		for method, program, calculation in calculations:
@@ -188,12 +181,9 @@ def _main(args, config, logger):
 		#args.calculations = shlex.split(silico.misc.tree.run(Node.from_list((known_methods, known_programs, known_calculations))))
 		args.calculations = shlex.split(Calculation_browser.run(Tree_node.from_configurable_lists((known_methods, known_programs, known_calculations))))
 	
-	# Get upset again if we have no calculations.
-	if len(args.calculations) == 0:
-		raise Silico_exception("No calculations to submit (use the -i option for interactive mode)")
-		#logger.error("No calculations to submit (use the -i option for interactive mode)")
-		#return 1
-		
+		# Get upset again if we have no calculations.
+		if len(args.calculations) == 0:
+			raise Silico_exception("No calculations to submit" + ("(use the -i option for interactive mode)" if not args.interactive else ""))
 	
 	try:
 		# Arrange our calcs into a linked list.

commit 5c992021295f45125bc6a58c9149a02025ae8587

Fixed some bizarre error messages introduced by 56f454a

silico/program/csubmit.py
@@ -178,12 +178,13 @@ def _main(args, config, logger):
 			break
 		
 		# Ask the user for calcs.
-		#args.calculations = shlex.split(silico.misc.tree.run(Node.from_list((known_methods, known_programs, known_calculations))))
 		args.calculations = shlex.split(Calculation_browser.run(Tree_node.from_configurable_lists((known_methods, known_programs, known_calculations))))
-	
-		# Get upset again if we have no calculations.
 		if len(args.calculations) == 0:
-			raise Silico_exception("No calculations to submit" + ("(use the -i option for interactive mode)" if not args.interactive else ""))
+			break
+	
+	# Get upset again if we have no calculations.
+	if len(args.calculations) == 0:
+		raise Silico_exception("No calculations to submit" + (" (use the -i option for interactive mode)" if not args.interactive else ""))
 	
 	try:
 		# Arrange our calcs into a linked list.