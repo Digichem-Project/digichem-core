fa6e8419ff5816985dc7a29430571191075b9511
oliver-s-lee <osl@st-andrews.ac.uk>
2020-10-29 11:06:01 +0000

    Changed purple to blue in urwid interfaces to improved readability.

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -18,8 +18,8 @@ description = "Silico Computational Chemistry Package"
 development = True
 # Version information.
 major_version = 0
-minor_version = 18
-revision = 4
+minor_version = 14
+revision = 5
 version_number = "{}.{}.{}".format(major_version, minor_version, revision)
 # The full version number of this package.
 version = "{}{}".format(version_number, "-dev" if development else "")
