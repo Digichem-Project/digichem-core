1b5efed3713c4236500c3d9e545a56f244b0bab3
oliver-s-lee <osl@st-andrews.ac.uk>
2020-10-31 14:41:53 +0000

    Added initial bash completion support.

---
+++ completion.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#
+# Helper script for tab completion.
+#
+
+_silico_complete() {
+	#echo "${#COMP_WORDS[@]}"
+	
+	if [ "$COMP_CWORD" -eq 1 ]; then
+		# First argument is a sub command name.
+		COMPREPLY=($(compgen -W "submit report result status" "${COMP_WORDS[1]}"))
+	else
+		COMPREPLY=($(compgen -f "${COMP_WORDS[$COMP_CWORD]}"))
+	fi
+}
+
+complete -F _silico_complete silico
+
+# _silico_dev_complete() {
+# 	
+# }
+# 
+# complete -F _silico_complete silico-dev
