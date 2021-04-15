35b49b88a379c06fbff3176c41fb2a8e1858a2b6
oliver-s-lee <osl@st-andrews.ac.uk>
2021-04-15 10:40:46 +0100

    Improved handling of emission energies; added support for emission energies of different multiplicity

--- digichem/result/emission.py
+++ digichem/result/emission.py
@@ -44,51 +44,158 @@ class Relaxed_excited_state(Excited_state):
         self.transition_type = transition_type
         
     @classmethod
-    def from_results(self, main_result, *, ground_state_result = None, excited_state_result, transition_type, excited_state = None):
+    def guess_from_results(self, *results):
         """
-        A more intelligent constructor for Relaxed_excited_state object.
+        Try and find emission energies from a number of calculation results.
+        
+        Attempts will be made to calculate both adiabatic and vertical emission energies based on the properties of the excited states in results.
+        
+        :param results: A number of result sets.
+        :returns: A tuple of dictionaries of the form (vertical, adiabatic), where the key of each dict is the multiplicity of the corresponding emission.
+        """        
+        # The two types of emission we consider.
+        # Each is a dictionary where the keys are multiplicities and the items are the corresponding emission.
+        adiabatic = {}
+        vertical = {}
+        
+        # We're only interested in optimisations (by definition emission needs to be from relaxed geom).
+        opt_results = [result for result in results if "Optimisation" in result.metadata.calculations]
+        
+        for excited_state_result in reversed(opt_results):
+            # See if we can use this calc type as an excited state.
+            # The easiest situation is if we have excited states in the calc (and it is an opt).
+            if len(result.excited_states) > 0:
+                # This will do fine for vertical.
+                vertical.update(self.for_each_multiplicity(excited_state_result, excited_state_result, "vertical"))
+                
+                # For adiabatic, we need the earliest suitable optimisation from our list that doesn't have excited states.
+                try:
+                    ground_state_result = [result for result in opt_results if len(result.excited_states) == 0][0]
+                    adiabatic.update(self.for_each_multiplicity(ground_state_result, excited_state_result, "adiabatic"))
+                except IndexError:
+                    # No suitable calcs.
+                    pass                    
+                    
+            else:
+                # We have no explicit excited states, but so long as we are an opt we can still find an emission if we have calcs of different mult.
+                # First look for adiabatic.
+                try:
+                    # Try and find a suitable ground.
+                    ground_state_result = [result for result in opt_results if result.metadata.multiplicity != excited_state_result.metadata.multiplicity][0]
+                    
+                    # Get 'emission' object.
+                    emission = self(ground_state_result, excited_state_result, "adiabatic")
+                    
+                    # If the energy is negative we will ignore.
+                    if emission.energy >= 0:
+                        adiabatic[excited_state_result.metadata.multiplicity] = emission
+                except IndexError:
+                    # No good.
+                    pass
+                
+                # And now vertical (we need a single point at different geom.
+                try:
+                    # Try and find a suitable ground.
+                    ground_state_result = [result for result in results if "Single Point" in result.metadata.calculations and result.metadata.multiplicity != excited_state_result.metadata.multiplicity][0]
+                    
+                    # Get 'emission' object.
+                    emission = self(ground_state_result, excited_state_result, "vertical")
+                    
+                    # If the energy is negative we will ignore.
+                    if emission.energy >= 0:
+                        vertical[excited_state_result.metadata.multiplicity] = emission
+                except IndexError:
+                    # No good.
+                    pass
+                
+        return (vertical, adiabatic)
+                
+                
+    @classmethod
+    def for_each_multiplicity(self, ground_state_result, excited_state_result, transition_type):
+        """
+        Return one emission object for each multiplicity in an excited state result.
+        
+        This method is useful because frequently only the lowest excited state of each mult is considered for emission, so there is one emission type per multiplicity. 
+        """
+        return {multiplicity: self(ground_state_result, excited_state_result, transition_type, states[0]) for multiplicity, states in excited_state_result.excited_states.group().items()}
+                
+        
+    @classmethod
+    def from_results(self, main_result, *, ground_state_result = None, excited_state_result, transition_type = None, excited_state = None):
+        """
+        Try and determine some emission energies from a number of result sets.
         
         :param main_result: A Result_set object containing main calculation results.
         :param ground_state_result: A Result_set object representing the ground state.
         :param excited_state_result: A Result_set object representing the excited state.
         :param transition_type:  A string describing the type of transition, either 'adiabatic' (GS and ES relaxed) or 'vertical' (ES relaxed, GS @ ES geom).
         :param excited_state: An optional Excited_state object. This is required (for example) in time dependent DFT where the total energy of excited_state_result is the ground state energy (at the excited state geometry). If excited_state is not an Excited_state object (and isn't None), it is passed as criteria to Excited_state_list.get_state(). 
-        """
-        # Decide on what to use for our ground state
+        """                
+        # Decide on what to use for our ground state if not given explicitly.
         if ground_state_result is None:
-            # If our excited results has TD excited states and we are a vertical transition, then it can be our ground state.
-            if len(excited_state_result.excited_states) > 0 and transition_type == "vertical":
-                ground_state_result = excited_state_result
-            elif transition_type == "adiabatic":
-                # Check we are an Opt, and complain if not.
-                if 'Optimisation' in main_result.metadata.calculations:
-                    # No TD excited states, so we'll use the main result object.
-                    ground_state_result = main_result
+            # If a transition type wasn't given, we can try and guess.
+            if transition_type is None:
+                # We don't know what type of emission we've been asked for.
+                # If the main_result is an opt, we'll assume we're adiabatic and use that as the ground state.
+                # Otherwise, we'll assume vertical and use the excited state.
+                if "Optimisation" in main_result.metadata.calculations:
+                    transition_type = "adiabatic"
                 else:
-                    # No explicit ground given and out main result is not an Opt, so it probably isn't suitable.            
-                    raise Silico_exception("Unable to determine ground state in adiabatic emission; no explicit ground state given and this calculation is not an optimisation")
+                    transition_type = "vertical"
+            
+            # Distinguish between the two types of excited states
+            if len(excited_state_result.excited_states) > 0:
+                # Emission from TD style excited states.
+                
+                if transition_type == "vertical":
+                    # For vertical emission, the excited state result is also the ground state result.
+                    ground_state_result = excited_state_result
+                    
+                elif transition_type == "adiabatic":
+                    # For adiabatic we need an optimised ground state geometry.
+                    if "Optimisation" in main_result.metadata.calculations:
+                        ground_state_result = main_result
+                        
+                    else:
+                        # No explicit ground given and out main result is not an Opt, so it probably isn't suitable.            
+                        raise Silico_exception("Unable to determine ground state in adiabatic emission; no explicit ground state given and this calculation is not an optimisation")
+                        
             else:
-                if 'Single Point' in main_result.metadata.calculations:
-                    ground_state_result = main_result
-                else:
-                    # Vertical transition via the unrestricted triplet method; we could (in theory) be the ground state, but might also be optimised ground (which would give adiabatic, not vertical).
-                    # Sadly, there's no real way of knowing for sure (even if we are a single point, it could be a single point at the optimised geom).
-                    # The best we do is guess; if we are a singlepoint, then we are OK as the ground.
-                    # If we are not; then we'll stop here. The user can always explicitly set this object as the ground if it is correct.
-                    # TODO: Maybe just convert to a warning?
-                    raise Silico_exception("Unable to determine ground state in vertical emission; no explicit ground state given and this calculation is not a single point")
+                # Emission from unrestricted triplet calc.
+                # This method requires another calc type to compare to.
+                if transition_type == "vertical":
+                    # For vertical, we wan't a singlet calc at the same geometry as the triplet calc.
+                    if 'Single Point' in main_result.metadata.calculations:
+                        ground_state_result = main_result
+                    else:
+                        raise Silico_exception("Unable to determine ground state in vertical emission; no explicit ground state given and this calculation is not a single point")
+                    
+                elif transition_type == "adiabatic":
+                    # For adiabatic, we need an opt.
+                    if "Optimisation" in main_result.metadata.calculations:
+                        ground_state_result = main_result
+                    else:
+                        # No explicit ground given and out main result is not an Opt, so it probably isn't suitable.            
+                        raise Silico_exception("Unable to determine ground state in adiabatic emission; no explicit ground state given and this calculation is not an optimisation")
             
         # Now decide which excited state to use.
         excited_state = excited_state if excited_state is None or isinstance(excited_state, Excited_state) else excited_state_result.excited_states.get_state(excited_state)
         
-        if excited_state is None and len(excited_state_result.excited_states) > 0:
-            excited_state = excited_state_result.excited_states[0]
+        # No excited state given and generating emission from TD style excited states, get all possible.
+        if len(excited_state_result.excited_states) > 0 and excited_state is None:
+            return self.for_each_multiplicity(ground_state_result, excited_state_result, transition_type)
         
-        return self(
-            ground_state_result,
-            excited_state_result,
-            transition_type,
-            excited_state)
+        else:
+            # Get emission.
+            emission = self(
+                ground_state_result,
+                excited_state_result,
+                transition_type,
+                excited_state)
+            
+            # Return as single dict.
+            return {emission.multiplicity: emission}
     
     @property
     def excited_energy(self):
