d15d49c5b95c353992125a74201a179bd197a7f1
oliver-s-lee <osl@st-andrews.ac.uk>
2021-10-12 10:39:18 +0100

    Refactoring of method browser

--- silico/interface/urwid/browser/base.py
+++ silico/interface/urwid/browser/base.py
@@ -6,52 +6,70 @@ from silico.interface.urwid.browser.calcbox import Calcbox
 
 
 
-class Calculation_browser():
+class Method_browser(urwid.TreeListBox):
     """
-    Class for displaying an urwid compatible calculation browser.
+    Class for browsing computational methods.
     """
-
-
-    def __init__(self, methods, palette):
+    
+    def __init__(self, top_node, selector):
         """
-        Constructor for calculation browser objects.
-        """
-        self.palette = palette
-        self.topnode = Loader_top_node(methods)
-        self.listbox = urwid.TreeListBox(urwid.TreeWalker(self.topnode))
-        self.listbox.offset_rows = 1
-        self.calcbox = Calcbox()
+        Constructor for Method_browser objects.
         
-        # Construct our display widgets.
-        self.view = Tab_pile([
-            ('weight', 2, urwid.LineBox(self.listbox, title = "Methods Browser", title_align = "left", title_attr = "bold")),
-            ('weight', 1, urwid.LineBox(self.calcbox, title = "Selected Methods", title_align = "left", title_attr = "bold")),
-            #(1, urwid.AttrMap(urwid.Filler(urwid.Padding(self.confirm, 'center', 11)), confirm_attr))
-        ])
+        :param top_node: The top most node to display.
+        :param selector: Method_selector object to add selected methods to.
+        """
+        super().__init__(urwid.TreeWalker(top_node))
+        self.selector = selector
 
-    def unhandled_input(self, key):
+    def keypress(self, size, key):
         if key in ['enter', ' ']:
             # First, get the focus.
-            focus_node = self.listbox.body.focus
+            focus_node = self.body.focus
             
             # If the node is a leaf, add it.
             # TODO: There might be a better way to ID leaf nodes...
             if not hasattr(focus_node, 'has_children'):
-                self.add_method(focus_node)
+                # Get the method of the node in focus.
+                method = focus_node.build_loader_path()
+                self.selector.add_method(method)
             
             else:
-                # Expand this parent node instead.
-                pass
+                # Do nothing.
+                super().keypress(size, key)
+        else:
+            # Do nothing.
+            super().keypress(size, key)
+    
+    
+
+class Method_selector(Tab_pile):
+    """
+    Class for displaying and selecting computational methods.
+    """
+
+
+    def __init__(self, methods):
+        """
+        Constructor for calculation browser objects.
+        """
+        self.topnode = Loader_top_node(methods)
+        self.listbox = Method_browser(self.topnode, self)
+        self.listbox.offset_rows = 1
+        self.calcbox = Calcbox()
+        
+        # Construct our display widgets.
+        super().__init__([
+            ('weight', 2, urwid.LineBox(self.listbox, title = "Methods Browser", title_align = "left", title_attr = "bold")),
+            ('weight', 1.5, urwid.LineBox(self.calcbox, title = "Selected Methods", title_align = "left", title_attr = "bold")),
+            #(1, urwid.AttrMap(urwid.Filler(urwid.Padding(self.confirm, 'center', 11)), confirm_attr))
+        ])
             
-    def add_method(self, focus):
+    def add_method(self, method):
         """
         Add the method that currently has focus to our calc box.
         
-        :param focus: The method that has focus currently.
-        """
-        # Get the method based on the focus node.
-        method = focus.build_loader_path()
-        
+        :param method: The method to add.
+        """        
         # Now add the method to our calcbox.
         self.calcbox.add_method(method)
 
