883125e96ace528e1c280e0bb82c7cc408d4e730
oliver-s-lee <osl@st-andrews.ac.uk>
2021-10-19 16:06:02 +0100

    Added new settings editor widgets and removed old classes

---
+++ silico/interface/urwid/setedit/base.py
@@ -0,0 +1,193 @@
+# Base code for Settings_browser widgets.
+
+# General imports.
+import urwid
+
+# Silico imports.
+from silico.interface.urwid.setedit.widget import Setedit_widget
+from silico.interface.urwid.base import Section
+
+
+class Setedit():
+    """
+    Logical storage for setting editors.
+    """
+    
+    def __init__(self, title, value, vtype, help = None):
+        """
+        
+        :param title: The title/name of this setedit.
+        :param value: The default value.
+        :param vtype: The type of value.
+        :param help: Optional help text to display.
+        """
+        self.title = title
+        self.value = value
+        self.vtype = vtype
+        self.help = help
+        self._widget = None
+    
+    def get_widget(self, reload = False):
+        """
+        Get the widget we'll use for display.
+        """
+        if self._widget is None or reload:
+            self._widget = self.load_widget()
+            
+        return self._widget
+    
+    def load_widget(self):
+        """
+        Load the widget we'll use for display.
+        """
+        return Setedit_widget.class_from_type(self.vtype)(self.title, self.value, self.help)
+    
+    def get_value(self):
+        """
+        Get the possibly edited value of this Setedit.
+        """
+        return self.get_widget().value
+    
+    @classmethod
+    def vtype_from_configurable_option(self, option):
+        """
+        Get the type of value expected by a configurable option.
+        
+        :param option: The configurable option.
+        :returns: The value type (a string).
+        """
+        try:
+            return option.type.__name__
+        
+        except Exception:
+            return "str"
+    
+    @classmethod
+    def value_from_configurable_options(self, owning_obj, option):
+        """
+        Get a dictionary of values from a Configurable options object.
+        
+        :param owning_obj: The owning object on which the Option object is set as a class attribute.
+        :param option: The configurable option.
+        """
+        values = {}
+        for sub_option in option.OPTIONS.values():
+            # If this sub option also has options, recurse.
+            if hasattr(sub_option, "OPTIONS"):
+                values[sub_option.name] = (self.value_from_options(owning_obj, sub_option), "Options", sub_option.help)
+                
+            else:
+                # A 'normal' option, get the value.
+                values[sub_option.name] = (sub_option.__get__(owning_obj), self.vtype_from_configurable_option(sub_option), sub_option.help)
+                
+        return values
+    
+    @classmethod
+    def from_configurable_option(self, owning_obj, option):
+        """
+        Construct a new Setedit object from a configurable option.
+        
+        :param owning_obj: The owning object on which the Option object is set as a class attribute.
+        :param option: The configurable option.
+        :returns: The Setedit object.
+        """
+        # First, keep an eye out for nested options.
+        if hasattr(option, "OPTIONS"):
+            # This option has options of its own.
+            return self(option.name, self.value_from_configurable_options(owning_obj, option), "Options", option.help)
+        
+        return self(option.name, option.__get__(owning_obj), self.vtype_from_configurable_option(option), option.help)    
+
+
+class Setedit_walker(urwid.SimpleFocusListWalker):
+    """
+    ListWalker-compatible class for displaying Settings.
+
+    Positions are Setedit objects, while actual display is handled by Setedit_widgets.
+    """
+
+    def __init__(self, data):
+        """
+        Constructor for Row_walker objects.
+        
+        :param data: List of data (Row_items) to display.
+        """
+        super().__init__(data)
+            
+    def get_focus(self):
+        """
+        Get the current focus.
+        
+        :returns: A tuple (widget, position) of the current focus.
+        """
+        try:
+            focus = self.focus
+            return self[focus].get_widget(), focus
+        except (IndexError, KeyError, TypeError):
+            return None, None
+        
+    def get_next(self, position):
+        """
+        Get the next.
+        
+        :returns: A tuple (widget, position) of position.
+        """
+        try:
+            position = self.next_position(position)
+            return self[position].get_widget(), position
+        except (IndexError, KeyError):
+            return None, None
+
+    def get_prev(self, position):
+        """
+        Get the previous.
+        
+        :returns: A tuple (widget, position) of position.
+        """
+        try:
+            position = self.prev_position(position)
+            return self[position].get_widget(), position
+        except (IndexError, KeyError):
+            return None, None
+
+
+class Setedit_browser(urwid.ListBox):
+    """
+    A widget that permits viewing and editing lists of options.
+    """
+    
+    def __init__(self, setedits):
+        super().__init__(Setedit_walker(setedits))
+        
+    @classmethod
+    def from_configurable(self, configurable):
+        """
+        Construct a Setedit_browser from a configurable object.
+        
+        :param configurable: A configurable object to construct from.
+        :returns: A setedit_browser object.
+        """        
+        setedits = [Setedit.from_configurable_option(configurable, option) for option in configurable.OPTIONS.values()]
+        return self(setedits)
+
+
+class Settings_editor(Section):
+    """
+    A high-level widget for editing a list of settings.
+    """
+    
+    def __init__(self, browser, title):
+        """
+        Constructor for Settings_editor objects.
+        
+        :param browser: A Setedit_browser to use as our body.
+        :param title: The title to show.
+        """
+        self.browser = browser
+        
+        super().__init__(self.browser, title)
+        
+        
+        
+    
+    
---
+++ silico/interface/urwid/setedit/widget.py
@@ -0,0 +1,441 @@
+# General imports.
+import urwid
+from silico.interface.urwid.misc import Blank
+
+# Silico imports.
+
+class Min_edit(urwid.Edit):
+    """
+    A fixed urwid edit widget that always leaves enough space for the cursor.
+    """
+    
+    def pack(self, *args, **kwargs):
+        """
+        Modified pack method to fix an urwid bug
+        """
+        packed = super().pack(*args, **kwargs)
+        # Always make sure we have enough space for our cursor.
+        return (packed[0] +1, packed[1])
+
+class Setedit_widget(urwid.Pile):
+    """
+    Top class for widgets that handle display of Setedit objects.
+    """
+    
+    title_attr = "bold"
+    edit_attr = "editable"
+    help_attr = "body"
+    
+    def __init__(self, widget_list, has_divider = True):
+        """
+        Constructor for Setedit_widget objects.
+        
+        :param widget_list: List of widgets to stack vertically.
+        :param has_divider: Whether to show a divider after this widget.
+        """
+        self.divider = urwid.Divider()
+        
+        if has_divider:
+            widget_list.append(self.divider)
+            
+        urwid.Pile.__init__(self, widget_list)
+    
+    @property
+    def value(self):
+        """
+        Get the possibly edited value of this widget.
+        """
+        raise NotImplementedError("Implement in subclass")
+    
+    def get_help_widget(self, help_text):
+        """
+        Return a widget used to display a help message.
+        """
+        return urwid.Text((self.help_attr, help_text if help_text is not None else ""))
+    
+    @classmethod
+    def value_to_str(self, value):
+        """
+        Convert a value to a string.
+        
+        This function handles mapping of 'None' values.
+        """
+        return str(value) if value is not None else ""
+    
+    @classmethod
+    def str_to_value(self, value):
+        """
+        Convert a string to a real value.
+        
+        This function handles mapping of 'None' values.
+        """
+        return value if value != "" else None
+    
+    @classmethod
+    def class_from_type(self, vtype):
+        """
+        Get a concrete Setedit_widget suitable for a certain type.
+        
+        :param vtype: The name of the type (as a string). If in doubt try type(value).__name__
+        :returns: A suitable class which will have a constructor of the form __init__(title, value(s), help).
+        """
+        if vtype == "bool":
+            return Bool_editor
+        
+        elif vtype == "list" or vtype == "tuple":
+            return List_editor
+        
+        elif vtype == "dict":
+            return Dict_editor
+        
+        elif vtype == "Options":
+            return Sub_setedit
+        
+        else:
+            return Text_editor
+
+
+class Single_editor(Setedit_widget):
+    """
+    A Setedit_widget for changing a single value.
+    """
+    
+    def __init__(self, edit_widget, help = None, has_divider = True):
+        """
+        Constructor for Setedit_widger objects.
+        
+        :param edit_widget: A widget to use for editing this settings.
+        :param help: Help text to display for this editor.
+        :param has_divider: Whether to show a divider after this widget.
+        """
+        self.edit_widget = edit_widget
+        
+        super().__init__([
+            urwid.AttrMap(self.edit_widget, self.edit_attr),
+            self.get_help_widget(help)
+        ], has_divider = has_divider)
+
+
+class Text_editor(Single_editor):
+    """
+    Editer widget for generic text types.
+    """
+    
+    def __init__(self, title, value, help = None, has_divider = True):
+        """
+        Constructor for Text_editor widgets.
+        
+        :param title: The title of this editor.
+        :param value: Default value of this editor.
+        :param help: Help text to display for this editor.
+        :param has_divider: Whether to show a divider after this widget.
+        """
+        super().__init__(urwid.Edit((self.title_attr, title + ": "), self.value_to_str(value)), help = help, has_divider = has_divider)
+    
+    @property
+    def value(self):
+        """
+        The currently, possibly edited, value of this widget.
+        """
+        value = self.edit_widget.get_edit_text()
+        return self.str_to_value(value)
+
+
+class Bool_editor(Single_editor):
+    """
+    Editer widget for bool values.
+    """
+    
+    def __init__(self, title, value, help = None, has_divider = True):
+        """
+        Constructor for Bool_editor widgets.
+        
+        :param title: The title of this editor.
+        :param value: Default value of this editor.
+        :param help: Help text to display for this editor.
+        :param has_divider: Whether to show a divider after this widget.
+        """
+        super().__init__(urwid.CheckBox((self.title_attr, title + ": "), value), help = help, has_divider = has_divider)
+    
+    @property
+    def value(self):
+        """
+        The current, possible edited, value of this widget.
+        """
+        return self.checkbox.get_state()
+
+
+class Text_list_editor(urwid.Edit):
+    """
+    Text editor that appears as part of a list.
+    """
+    
+    # Attribute that controls appearance of the position indicator.
+    position_attr = "body"
+    
+    def __init__(self, position, edit_text):
+        """
+        Constructor for Text_list_editor widgets.
+        
+        :param position: Starting position (index +1) of this editor.
+        :param edit_text: Default value of this editor.
+        """
+        
+        # Replace None values with actual text.
+        edit_text = Setedit_widget.value_to_str(edit_text)
+                
+        super().__init__("", edit_text)
+        
+        # Set our position.
+        self.set_position(position)
+        
+    def set_position(self, position):
+        """
+        Tell this widget where it appears in the list.
+        """
+        self.set_caption((self.position_attr, "{}) ".format(position)))
+    
+    def pack(self, *args, **kwargs):
+        """
+        Modified pack method to fix an urwid bug
+        """
+        packed = super().pack(*args, **kwargs)
+        # Always make sure we have enough space for our cursor.
+        return (packed[0] +1, packed[1])
+    
+    @property
+    def value(self):
+        """
+        The currently, possible edited, value of this widget.
+        """
+        value = self.get_edit_text()
+        return Setedit_widget.str_to_value(value)
+
+
+class List_editor(Setedit_widget):
+    """
+    Editor widget for list values.
+    """
+    
+    def __init__(self, title, values, help = None, has_divider = True, strip_empty = True):
+        """
+        Constructor for Bool_editor widgets.
+        
+        :param title: The title of this editor.
+        :param values: List of starting values.
+        :param help: Help text to display for this editor.
+        :param has_divider: Whether to show a divider after this widget.
+        :param strip_empty: Whether to remove empty values in the list.
+        """
+        self.strip_empty = strip_empty
+        
+        # An inner pile we'll use for adding/removing values.
+        self.inner_pile = urwid.Pile(self.get_fields(values))
+        
+        super().__init__([
+            # Widget for our title.
+            urwid.Text((self.title_attr, title)),
+            # Help text.
+            self.get_help_widget(help),
+            # Actual contents.
+            self.inner_pile
+        ], has_divider = has_divider)
+                
+    def get_fields(self, values):
+        """
+        Build a list of edit fields based on a value.
+        """
+        fields = [self.get_field(index +1, value) for index, value in enumerate(values)]
+        
+        # Add one final editor so we can add more values.
+        fields.append(self.get_field(len(fields) +1, None))
+        
+        return fields
+        
+    def get_field(self, position, value):
+        """
+        Get a new editable field widget.
+        
+        :param position: The position of the new field in the list.
+        :param value: The starting position of value.
+        :returns: The new widget
+        """
+        field =  Text_list_editor(position, edit_text = value)
+        
+        # Add a signal to watch for changes.
+        urwid.connect_signal(field, 'postchange', lambda *data: self.adjust())
+        
+        return urwid.AttrMap(field, self.edit_attr)
+    
+    @property
+    def value(self):
+        """
+        The currently, possible edited, value of this widget.
+        """
+        return [field.base_widget.value for field, options in self.inner_pile.contents if field.value is not None or not self.strip_empty]
+        
+    def adjust(self):
+        """
+        Adjust the length of this list editor.
+        """
+        # Check to see if the last field has something in it or not.
+        if self.inner_pile.contents[-1][0].base_widget.value is not None:
+            # Add new final editor.
+            self.inner_pile.contents.append((self.get_field(len(self.contents) +1, None), self.options()))
+
+        elif len(self.inner_pile.contents) > 1 and self.inner_pile.contents[-2][0].base_widget.value is None:
+            # We have too many empty editors, remove the last.
+            self.inner_pile.contents.pop()
+                        
+            # Check again.
+            self.adjust()
+            
+        # Update positions.
+        for index, (field, options) in enumerate(self.inner_pile.contents):
+            field.base_widget.set_position(index +1)
+
+
+class Dict_item_editor(urwid.Columns):
+    """
+    Editor that appears as part of dictionary.
+    """
+    
+    edit_attr = "editable"
+    body_attr = "body"
+    
+    def __init__(self, key, value, change_callback = None):
+        """
+        Constructor for Dict_item_editor objects.
+        
+        :param key: The key of this dictionary item.
+        :param value: The value of this dictionary item.
+        :param change_callback: Function to call when this editor changes value.
+        """
+        # We have two separate editable entities.
+        self.key_edit = Min_edit(edit_text = Setedit_widget.value_to_str(key))
+        self.value_edit = Min_edit(edit_text = Setedit_widget.value_to_str(value))
+                
+        # Add a signal to watch for changes.
+        urwid.connect_signal(self.key_edit, 'postchange', lambda *data: change_callback())
+        urwid.connect_signal(self.value_edit, 'postchange', lambda *data: change_callback())
+                
+        # Call parent.
+        super().__init__([
+            ('pack', urwid.AttrMap(self.key_edit, self.edit_attr)),
+            ('pack', urwid.Text(": ")),
+            ('pack', urwid.AttrMap(self.value_edit, self.edit_attr))],
+            min_width = 5
+        )
+    
+    @property
+    def value(self):
+        """
+        Get the possibly edited value of this field.
+        """
+        return (Setedit_widget.str_to_value(self.key_edit.get_edit_text()), Setedit_widget.str_to_value(self.value_edit.get_edit_text()))
+        
+
+
+class Dict_editor(List_editor):
+    """
+    Editor widget for dicts
+    """
+        
+    def get_field(self, key, value):
+        """
+        Get a new editable field widget.
+        
+        :param key: The key of this dictionary item.
+        :param value: The value of this dictionary item.
+        """
+        return Dict_item_editor(key, value, self.adjust)
+    
+    def get_fields(self, values):
+        """
+        Build a list of edit fields based on a value.
+        """
+        fields = [self.get_field(key, value) for key, value in values.items()]
+        
+        # Add one final editor so we can add more values.
+        fields.append(self.get_field(None, None))
+        
+        return fields
+                    
+    @property
+    def value(self):
+        """
+        The currently, possible edited, value of this widget.
+        """
+        val =  {}
+        for field, options in self.inner_pile.contents:
+            key, value = field.value
+            if not (key is None and value is None) or not self.strip_empty:
+                val[key] = value
+                
+        return val
+
+    def adjust(self):
+        """
+        Adjust the length of this list editor.
+        """
+        # Check to see if the last field has something in it or not.
+        if self.inner_pile.contents[-1][0].value != (None, None):
+            # Add new final editor.
+            self.inner_pile.contents.append((self.get_field(None, None), self.options()))
+
+        elif len(self.inner_pile.contents) > 1 and self.inner_pile.contents[-2][0].value == (None, None):
+            # We have too many empty editors, remove the last.
+            self.inner_pile.contents.pop()
+                        
+            # Check again.
+            self.adjust()
+
+
+class Sub_setedit(Setedit_widget):
+    """
+    A Setedit object for editing sub options.
+    """
+    
+    def __init__(self, title, values, help = None, has_divider = True):
+        """
+        Constructor for Sub_setedit objects.
+        
+        :param title: The title of this editor.
+        :param values: A dictionary of values. The key for each item is the name of that sub option, while the value is a tuple of (value, type, help), where type indicates the type of that sub option.
+        :param help: Help text to display for this editor.
+        :param has_divider: Whether to show a divider after this widget.
+        """
+        self.inner_pile = urwid.Pile(self.get_fields(values))
+        
+        super().__init__([
+            # Widget for our title.
+            urwid.Text((self.title_attr, title)),
+            # Help text.
+            self.get_help_widget(help),
+            # Actual contents.
+            urwid.Padding(self.inner_pile, left = 2)
+        ], has_divider = has_divider)
+        
+    def get_fields(self, values):
+        """
+        :param values: A dictionary of values. The key for each item is the name of that sub option, while the value is a tuple of (value, type, help), where type indicates the type of that sub option.
+        """
+        fields = []
+        
+        for key, (value, vtype, help) in values.items():
+            fields.append(self.class_from_type(vtype)(key, value, help, has_divider = len(fields) != len(values)-1))
+            
+        return fields
+    
+    @property
+    def value(self):
+        """
+        The currently, possible edited, value of this widget.
+        """
+        val =  {}
+        for field, options in self.inner_pile.contents:
+            key, value = field.value
+            val[key] = value
+                
+        return val
+
