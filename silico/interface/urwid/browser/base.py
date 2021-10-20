6feb10723b22c1346f02c545c228b2bc87fb8c22
oliver-s-lee <osl@st-andrews.ac.uk>
2021-10-20 15:02:36 +0100

    Added settings to method browser

--- silico/interface/urwid/browser/base.py
+++ silico/interface/urwid/browser/base.py
@@ -1,8 +1,9 @@
 import urwid
 
 from silico.interface.urwid.browser.node import Loader_top_node
-from silico.interface.urwid.base import Section
 from silico.interface.urwid.tree.base import Flaggable_tree_list_box
+from silico.config.configurable.option import Option
+from silico.interface.urwid.top import View
     
 
 class Method_browser(Flaggable_tree_list_box):
@@ -16,21 +17,24 @@ class Method_browser(Flaggable_tree_list_box):
         Constructor for calculation browser objects.
         """
         topnode = Loader_top_node(methods)
-        super().__init__(urwid.TreeWalker(topnode))
+        super().__init__(urwid.TreeWalker(topnode))    
 
 
-class Method_selector(Section):
+class Method_selector(View):
     """
     A tree list box widget used to browse and select methods.
     """
+    
+    show_hidden = Option(help = "Whether to show hidden methods.", default = False, type = bool)
 
     def __init__(self, methods):
         """
-        Constructor for File_selector objects.
+        Constructor for Method_selector objects.
         
-        :param starting_dir: The starting directory that will be shown expanded.
+        :param methods: The methods that can be selected.
         """
         self.browser = Method_browser(methods)
         self.browser.offset_rows = 1
         
         super().__init__(self.browser, "Method Browser")
+        
