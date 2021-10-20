e58506cd68a7d8ba5c829cd67437662677fc7883
oliver-s-lee <osl@st-andrews.ac.uk>
2021-10-20 15:02:12 +0100

    Tweaks to configurable editor widgets

--- silico/interface/urwid/configurable.py
+++ silico/interface/urwid/configurable.py
@@ -1,7 +1,6 @@
 # Code for editing configurables.
 
 # General imports.
-import urwid
 
 # Silico imports.
 from silico.interface.urwid.setedit.base import Settings_editor, Setedit_browser
@@ -13,10 +12,9 @@ class Configurable_editor(Settings_editor):
     A high-level widget for changing the settings of a configurable.
     """
     
-    def __init__(self, configurable):
+    def __init__(self, configurable, title):
         self.configurable = configurable
-        
-        super().__init__(Setedit_browser.from_configurable(configurable), "Editing {} with ID: {}".format(configurable.TYPE, configurable.index()))
+        super().__init__(Setedit_browser.from_configurable(configurable), title)
     
 
 class Method_editor(Pages):
@@ -29,7 +27,13 @@ class Method_editor(Pages):
         """
         # We show in reverse order, because typically the calculation is what the user wants to change anyway.
         super().__init__([
-            ("Calculation", Configurable_editor(calculation)),
-            ("Program", Configurable_editor(program)),
-            ("Destination", Configurable_editor(destination))
+            ("Calculation", Configurable_editor(calculation, self.get_title(calculation))),
+            ("Program", Configurable_editor(program, self.get_title(program))),
+            ("Destination", Configurable_editor(destination, self.get_title(destination)))
         ])
+        
+    def get_title(self, method_target):
+        """
+        Get the title to display for a given method_target.
+        """
+        return "Editing {} with ID: {}".format(method_target.TYPE, method_target.index())
