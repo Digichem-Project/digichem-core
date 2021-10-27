b87713468ef03c88584f5a4eca7558c5305d1420
oliver-s-lee <osl@st-andrews.ac.uk>
2021-10-27 09:27:53 +0100

    Prevented destinations and programs from being accidentally selected in the method browser; added method to call when settings are changed

--- silico/interface/urwid/browser/base.py
+++ silico/interface/urwid/browser/base.py
@@ -12,12 +12,21 @@ class Method_browser(Flaggable_tree_list_box):
     """
 
 
-    def __init__(self, methods):
+    def __init__(self, methods, show_hidden):
         """
         Constructor for calculation browser objects.
         """
-        topnode = Loader_top_node(methods)
-        super().__init__(urwid.TreeWalker(topnode))    
+        self.topnode = Loader_top_node(methods, show_hidden = show_hidden)
+        super().__init__(urwid.TreeWalker(self.topnode))
+        
+    def is_selectable(self, node):
+        """
+        Determine whether a given node is selectable.
+        """
+        # Only base calculations are selectable.
+        # We do this extra check incase there are destinations and/or programs without children.
+        loader_list = node.get_value()        
+        return not loader_list[-1].partial and node.loader_type == "calculation"
 
 
 class Method_selector(View):
@@ -33,8 +42,17 @@ class Method_selector(View):
         
         :param methods: The methods that can be selected.
         """
-        self.browser = Method_browser(methods)
+        self.browser = Method_browser(methods, show_hidden = self.show_hidden)
         self.browser.offset_rows = 1
+        super().__init__(self.browser, title = "Method Browser")
         
-        super().__init__(self.browser, "Method Browser")
+    def on_settings_change(self):
+        """
+        A method that will be called when settings have been changed.
+        """
+        # We need to update whether we're showing hidden stuff.
+        self.browser.topnode._show_hidden = self.show_hidden
+        self.browser.topnode.refresh()
+        # TODO: This is in case a node was removed which was in focus, there is probably a better solution.
+        self.browser.body.set_focus(self.browser.topnode)
         
