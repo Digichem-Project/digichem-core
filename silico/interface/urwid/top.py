f9f742f315b7183ab84f64d873ffda15843c5d97
oliver-s-lee <osl@st-andrews.ac.uk>
2021-11-04 14:42:44 +0000

    Added functionality for swapping top widget behind an overlay

--- silico/interface/urwid/top.py
+++ silico/interface/urwid/top.py
@@ -8,6 +8,8 @@ from silico.interface.urwid.setedit.view import View_browser
 # General imports.
 import urwid
 from urwid.widget import WidgetWrap
+from silico.interface.urwid.dialogue import Dialogue
+from urwid.decoration import WidgetPlaceholder
 
 
 class Top(urwid.WidgetPlaceholder):
@@ -22,14 +24,35 @@ class Top(urwid.WidgetPlaceholder):
         :param original_widget: The main widget to wrap around. Must be given (use a placeholder if you require).
         """
         super().__init__(original_widget)
+        
+        self.current_top = WidgetPlaceholder(original_widget)
+        
         self.stack = []
+        # A widget we'll use to popup and show program output.
+        self.output_widget = Output_widget(self)
+        
+    def output(self, message, error = False):
+        """
+        Add text to the output widget and show it on top.
+        
+        :param message: The text to add.
+        :param error: Whether to show alternative formatting indicating an error.
+        """
+        # First, add our message to our widget.
+        self.output_widget.output(message, error)
+        
+        # Then swap to it if it's not already the top.
+        if self.original_widget != self.output_widget:
+            self.swap(self.output_widget)
 
     def swap(self, original_widget):
         """
         Set a new widget as the top-most.
         """
         self.stack.append(self.original_widget)
+        self.current_top.original_widget = self.original_widget if original_widget == self.output_widget else original_widget
         self.original_widget = original_widget
+        
 
     def swap_into_window(self, original_widget, cancel_callback = None, submit_callback = None):
         """
@@ -55,18 +78,28 @@ class Top(urwid.WidgetPlaceholder):
         
         self.swap(window)
 
-    def back(self, number = 1):
+    def back(self, number = 1, force = False):
         """
         Remove the current top-most widget and set the last in its place.
         
         If there are no more widgets to go back to, urwid.ExitMainLoop will be raised.
         
         :param number: The number of times to go back.
+        :param force: Whether to force switching back even if the current top is the output_widget (which is normally sticky).
         :return: The widget just removed, for convenience.
         """
         for iter in range(0, number):
-            try:
-                self.original_widget = self.stack.pop()
+            try:                    
+                if self.original_widget == self.output_widget and force or self.original_widget != self.output_widget:
+                    # Remove the output widget.
+                    self.original_widget = self.stack.pop()
+                    self.current_top.original_widget = self.original_widget
+                    
+                else:
+                    # Go back without the output widget (which is current original_widget).
+                    stack_top = self.stack.pop()
+                    self.current_top.original_widget = self.stack[-1]
+                
             except IndexError:
                 # The stack is empty.
                 raise urwid.ExitMainLoop()
@@ -82,6 +115,56 @@ class Top(urwid.WidgetPlaceholder):
             self.back()
         
         return key
+
+
+class Output_widget(Dialogue):
+    """
+    A widget that pops up to display stdout and stderr.
+    """
+    
+    def __init__(self, top):
+        """
+        Constructor for Confirm dialogue boxes.
+        
+        :param top: The top widget used for display.
+        """                        
+        super().__init__(title = "Program Output", top = top)
+        
+    def output(self, message, error = False):
+        """
+        Add output text to this widget.
+        
+        :param message: The output text to add.
+        :param error: Whether to show alternative formatting to indicate an error.
+        """
+        # Decide on formatting.
+        if error:
+            # Error formatting.
+            attr = "section--dialogue--error"
+        
+        else:
+            # Normal formatting.
+            attr = "dialogue"
+        
+        # Add text.
+        for sub_message in message.splitlines():
+            new_text = urwid.Text((attr, sub_message))
+            self.list.body.append(new_text)
+        
+        # Scroll to bottom.
+        self.list.body.focus = len(self.list.body) -1
+        
+    def close(self):
+        """
+        """
+        self.topmost.back(force = True)
+        return False
+        
+    def get_body(self):
+        """
+        Get the outer widget we'll use for display.
+        """
+        return Confirm(self.get_inner(), self.topmost, submit_callback = self.close)
 
 
 class View(WidgetWrap, Configurable):
