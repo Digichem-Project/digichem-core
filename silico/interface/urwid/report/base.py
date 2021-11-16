266f330a8e4e98c77c6cb343ac5df8e5720f7b32
oliver-s-lee <osl@st-andrews.ac.uk>
2021-11-16 17:27:33 +0000

    Removed unused defaults.

--- silico/interface/urwid/report/base.py
+++ silico/interface/urwid/report/base.py
@@ -16,13 +16,13 @@ class Report_generator(Program_view):
     Class for controlling interface to reports generator.
     """
     
-    output = Option(help = "Base directory to which output will be written.", type = Path, default = "./")
-    name = Option(help = "Name of the molecule/system to use in the report.", type = str, default = None)
-    report_type = Option(help = "The type of report to render.", type = str, default = "full", choices = ["full", "atoms"])
-    render_style = Option(help = "The visual style with which to render orbital and molecular images.", type = str, choices=['pastel', 'light-pastel', 'dark-pastel','sharp', 'gaussian', 'vesta'], default = "pastel")
-    render_images = Option("render", help = "Whether to render new images.", type = bool, default = True)
-    overwrite = Option(help = "Whether to overwrite existing image files. Useful for forcing re-rendering, for example if the render_style has been changed.", type = bool, default = False)
-    auto_crop = Option(help = "Whether to automatically crop rendered images to remove excess whitespace. If false, rendered images will have the same resolution, but molecules may only occupy a small portion of the true image", type = bool, default = True)
+    output = Option(help = "Base directory to which output will be written.", type = Path,)
+    name = Option(help = "Name of the molecule/system to use in the report.", type = str)
+    report_type = Option(help = "The type of report to render.", type = str, choices = ["full", "atoms"])
+    render_style = Option(help = "The visual style with which to render orbital and molecular images.", type = str, choices=['pastel', 'light-pastel', 'dark-pastel','sharp', 'gaussian', 'vesta'])
+    render_images = Option("render", help = "Whether to render new images.", type = bool)
+    overwrite = Option(help = "Whether to overwrite existing image files. Useful for forcing re-rendering, for example if the render_style has been changed.", type = bool)
+    auto_crop = Option(help = "Whether to automatically crop rendered images to remove excess whitespace. If false, rendered images will have the same resolution, but molecules may only occupy a small portion of the true image", type = bool)
     
     
     def __init__(self, window, program):
