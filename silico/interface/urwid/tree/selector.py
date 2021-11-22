e300e2ceffdafc8d7d72d62f6c60055862ec0291
oliver-s-lee <osl@st-andrews.ac.uk>
2021-11-22 18:03:30 +0000

    Added new selector ABC class for more useful tree widgets

---
+++ silico/interface/urwid/tree/selector.py
@@ -0,0 +1,54 @@
+from silico.interface.urwid.top import View
+from silico.interface.urwid.misc import Tab_pile
+from silico.interface.urwid.section import Section
+import urwid
+
+
+class Selector(View):
+    """
+    ABC for widgets that contain a tree widget for selecting something.
+    """
+    
+    def __init__(self, browser, manual_widget, title, manual_widget_title):
+        """
+        Constructor for Selector objects.
+        """
+        self.browser = browser
+        self.manual_widget = manual_widget
+        self.manual_widget_title = manual_widget_title
+        
+        super().__init__(self.browser, title)
+        
+    def _get_body(self):
+        """
+        Get the widget we'll use for display.
+        """
+        main_body = super()._get_body()
+        
+        # We will wrap our body in a Pile and add our manual widget underneath in.
+        return Tab_pile([
+            main_body,
+            (3, Section(urwid.Filler(urwid.AttrMap(self.manual_widget, "editable")), self.manual_widget_title))
+        ])
+        
+    @property
+    def selected(self):
+        """
+        A property resolving to the values that are currently selected.
+        """
+        selected = self.browser.selected
+        
+        # Also add the value from our manual edit widget (if not empty).
+        if self.manual_widget.get_edit_text() != "":
+            selected.append(self.manual_widget.get_edit_text())
+            
+        return selected
+    
+    def reset(self):
+        """
+        Reset this selector so nothing is currently selected.
+        """
+        self.browser.reset()
+        
+        # Also reset our manual widget.
+        self.manual_widget.set_edit_text("")
