9cf7587cdebca369929bb96a77d43b12bfc1ec04
oliver-s-lee <osl@st-andrews.ac.uk>
2021-12-08 22:21:55 +0000

    Temporarily (hopefully) removed multiprocess parsing of results in the interactive result program (because it breaks logging to urwid)

--- silico/interface/urwid/result/list.py
+++ silico/interface/urwid/result/list.py
@@ -5,8 +5,8 @@ import urwid
 from silico.interface.urwid.row_list.base import Row_browser, Row_item,\
     Row_widget
 from silico.interface.urwid.result.browser import Result_selector
-import silico
-from silico.parser.base import parse_multiple_calculations
+import silico.logging
+from silico.parser.base import parse_multiple_calculations, parse_calculation
 from silico.result.alignment.base import Alignment
 
 
@@ -25,9 +25,9 @@ class Result_widget(Row_widget):
         Load the widget we'll use to display our main body.
         """
         return urwid.Columns([
-            urwid.Text(self.row_item._value.metadata.identity_string),
+            ('weight', 1, urwid.Text(self.row_item._value.metadata.identity_string)),
             # TODO: Careful, what if result has no file?
-            urwid.Text(self.row_item._value.metadata.log_files[0])
+            ('weight', 2, urwid.Text(str(self.row_item._value.metadata.log_files[0])))
         ])
 
 
@@ -58,7 +58,7 @@ class Result_list(Row_browser):
     A widget for displaying a list of parsed results.
     """
     
-    def __init__(self, top, start_dir = None, rearrangeable = True, initial_results = None):
+    def __init__(self, top, start_dir = None, rearrangeable = True, initial_results = None, subprocess_init = None):
         """
         Constructor for Result_list objects.
         
@@ -66,8 +66,10 @@ class Result_list(Row_browser):
         :param start_dir: The directory to show when first opening the file selector.
         :param rearrangeable: Whether the list of loaded results can be rearranged by the user.
         :param initial_results: A list of parsed results to show by default.
+        :param subprocess_init: A function to call to initialise subprocesses when parsing results in parallel.
         """
         top = top
+        self.subprocess_init = subprocess_init
         
         # Widget for choosing files, stored at self.selector
         file_selector = Result_selector(top, start_dir, title = "Select Result Files")
@@ -82,14 +84,16 @@ class Result_list(Row_browser):
         Add selected files to our list.
         """
         # Get the logger item we'll use for communication.
-        logger = silico.get_logger()
+        logger = silico.logging.get_logger()
         logger.info("Parsing calculation results")
         
         # Get the chosen alignment class.
         alignment = Alignment.from_class_handle(self.selector.alignment)
         
         # Parse (in parallel).
-        results = parse_multiple_calculations(*self.selector.selected, alignment_class = alignment, init_func = self.program.subprocess_init, processes = self.selector.num_CPUs)
+        # We currently do not do this in parallel because logging to urwid gets messed up...
+        #results = parse_multiple_calculations(*self.selector.selected, alignment_class = alignment, init_func = self.subprocess_init, processes = self.selector.num_CPUs)
+        results = [parse_calculation(selected, alignment_class = alignment) for selected in self.selector.selected]
         
         # Add each.
         for result in results:
