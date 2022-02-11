7d5aa0eb0db821ac45e330d9157f061283cb3488
oliver-s-lee <osl@st-andrews.ac.uk>
2022-02-11 09:16:12 +0000

    Added custom argparse extend action class for python versions where it is not supported natively

--- digichem/misc/argparse.py
+++ digichem/misc/argparse.py
@@ -17,28 +17,3 @@ class Extend_action(argparse.Action):
             setattr(namespace, self.dest, attr)
             
         attr.extend(values)
-
-# TODO: Unused.
-class List_grouper(argparse.Action):
-    """
-    Custom action class that groups lists together so we know in what order they were specified.
-    """    
-    
-    def __init__(self, option_strings, *args, **kwargs):
-        """
-        """
-        argparse.Action.__init__(self, option_strings, *args, **kwargs)
-        # We'll give ourself a name so we also group no matter which of our option_strings is used.
-        self.name = [name for name in option_strings if name[:2] == "--"]
-    
-    def __call__(self, parser, namespace, values, option_string=None):
-        """
-        """
-        grouped_list = {
-            'group': self,
-            'values': values
-        }
-        try:
-            getattr(namespace, self.dest).append(grouped_list)
-        except AttributeError:
-            setattr(namespace, self.dest, [grouped_list])
