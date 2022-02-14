b08ec7039caaf25535a16a50ca787c84e87d8dc2
oliver-s-lee <osl@st-andrews.ac.uk>
2022-02-14 13:43:25 +0000

    Bumped pre-release number

--- silico/__init__.py
+++ silico/__init__.py
@@ -21,7 +21,7 @@ description = "Silico Computational Chemistry Package"
 major_version = 1
 minor_version = 0
 revision = 0
-prerelease = 5
+prerelease = 6
 # Whether this is a development version.
 development = prerelease is not None
 # The full version number of this package.
