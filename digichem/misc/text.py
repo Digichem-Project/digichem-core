55d1464951321f618003eced2d5411fa3659f4df
oliver-s-lee <osl@st-andrews.ac.uk>
2022-03-11 13:27:46 +0000

    Added silico.misc.text.andjoin() function

---
+++ digichem/misc/text.py
@@ -0,0 +1,20 @@
+
+def andjoin(listlike):
+    """
+    Joins a list of string-like variables with commans, except for the last item which is joined with 'and'.
+    
+    For example:
+    >>> andjoin(["item1"])
+    "item1"
+    >>> andjoin(["item1", "item2"])
+    "item1 and item2"
+    >>> andjoin(["item1", "item2", "item3"])
+    "item1, item2 and item3"
+    """
+    # Now combine.
+    joined = ", ".join(str(listlike[:-1]))
+    
+    if len(listlike) > 1:
+        joined += " and " + str(listlike[-1])
+        
+    return joined
