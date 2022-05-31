2bca79e1ecd43db93919bca8ff499375016af89d
oliver-s-lee <osl@st-andrews.ac.uk>
2022-05-31 16:06:28 +0100

    Bumped pre-release number

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -21,7 +21,7 @@ description = "Silico Computational Chemistry Package"
 major_version = 1
 minor_version = 0
 revision = 0
-prerelease = 26
+prerelease = 27
 # Whether this is a development version.
 development = False
 # Version information.
