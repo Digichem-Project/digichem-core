30b3ae1da9a3196d7ba0e702c7cea637cdc26bc1
oliver-s-lee <osl@st-andrews.ac.uk>
2022-06-09 09:42:34 +0100

    Various minor refactoring to top-level package: removed unused base.py, renamed version to __version__, removed unnecessary name and description attributes, other minor changes

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -1,22 +1,18 @@
-# General imports.
+"""Computational chemistry management"""
+
 from datetime import datetime
 from pathlib import Path
 import pkg_resources
 import os
 import sys
-import PIL.Image
 
-# Silico imports.
 import silico.logging
-from .base import init_obabel
 
 
-PIL.Image.MAX_IMAGE_PIXELS = None
+####################
+# Package metadata.#
+####################
 
-# The name of this package.
-name = "silico"
-# Brief description.
-description = "Silico Computational Chemistry Package"
 # Version information.
 major_version = 1
 minor_version = 0
@@ -30,24 +26,43 @@ minor_version = 20
 revision = 5
 version_number = "{}.{}.{}".format(major_version, minor_version, revision)
 # The full version number of this package.
-version = "{}.{}.{}{}".format(major_version, minor_version, revision, "-pre.{}".format(prerelease) if development else "")
-# The bloke who wrote this.
-author = "Oliver Lee"
-# Program date (when we were last updated).
-_last_updated_string = "11/02/2022"
+__version__ = "{}.{}.{}{}".format(major_version, minor_version, revision, "-pre.{}".format(prerelease) if development else "")
+# Deprecated:
+version = __version__
+
+# The blokes who wrote this.
+__author__ = "The Silico Dev Team"
+
+# Program date (when we were last updated). This is changed automatically.
+_last_updated_string = "12/12/1234"
 last_updated = datetime.strptime(_last_updated_string, "%d/%m/%Y")
 
-# Decide on whether we are frozen or not.
+
+# Set-up openbabel.
+#
+# This must be done prior to an obabel import.
+#     
+# When frozen with pyinstaller we take a version of the openbabel C library with us (along with the relevant python bindings of course).
+# This library is split into several .so files corresponding to the various formats obabel supports, and while the main libopenbabel.so file is found automatically, these supplementary library files are not.
+# So, when we are frozen, we manually set the location of these library files so openbabel will work.
+# If we are not frozen we do not do this as we expect openbabel to be correctly configured.
 # The sys attribute 'frozen' is our flag, '_MEIPASS' is the dir location.
 # https://pyinstaller.readthedocs.io/en/stable/runtime-information.html#run-time-information
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     frozen = True
 else:
     frozen = False
+
+openbabel_version = "3.0.0"
+# The sys attribute 'frozen' is our flag, '_MEIPASS' is the dir location.
+# https://pyinstaller.readthedocs.io/en/stable/runtime-information.html#run-time-information
+if silico.frozen:
+    # We need to tell openbabel where its library components are.
+    os.environ['BABEL_LIBDIR'] = str(Path(sys._MEIPASS, "openbabel", "lib", openbabel_version))
     
-# Setup openbabel library location.
-init_obabel()
-    
+    # And also data.
+    os.environ['BABEL_DATADIR'] = str(Path(sys._MEIPASS, "openbabel", "data", openbabel_version))
+
 
 ########################
 # Function Definitions #
