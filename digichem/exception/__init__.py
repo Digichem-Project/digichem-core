c85375ff5179dfdbaead7bc8467f4b5e6c8c5fcb
oliver-s-lee <osl@st-andrews.ac.uk>
2022-06-10 09:52:07 +0100

    Renamed old Extractor_error exception class to Format_error

--- digichem/exception/__init__.py
+++ digichem/exception/__init__.py
@@ -6,6 +6,6 @@ from .base import Result_unavailable_error
 from .base import File_maker_exception
 from .base import Unknown_file_type_exception
 from .base import Submission_error
-from .base import Extractor_error
+from .base import Format_error
 from silico.exception.configurable import Configurable_exception
 from silico.exception.configurable import Missing_option_exception
