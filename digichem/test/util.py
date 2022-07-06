9bd1e4acb8181cf416a8ffc508d5225b47e184a4
oliver-s-lee <osl@st-andrews.ac.uk>
2022-07-06 12:08:18 +0100

    Moved check_dipole helper function to util module

--- digichem/test/util.py
+++ digichem/test/util.py
@@ -11,3 +11,13 @@ def check_float_list(test_list, expected_list, abs = 1e-4):
     #assert test_list == pytest.approx(expected_list, abs=abs)
     for list_index in range(0,len(expected_list)):
         assert test_list[list_index] == pytest.approx(expected_list[list_index], abs=abs)
+
+
+def check_dipole(dipole_moment, coords):
+    """
+    Helper function to check a dipole moment (PDM, TEDM or TMDM) matches some expected values.
+    """
+    check_float_list(dipole_moment.vector_coords, coords)
+        
+    # Check total
+    assert dipole_moment.total == pytest.approx((coords[0] **2 + coords[1] **2 + coords[2] **2) **0.5, abs=1e-4)
