b9eb6941683b9c8dc5519a5cc2ef747b620b1f41
oliver-s-lee <osl@st-andrews.ac.uk>
2022-07-22 17:59:33 +0100

    Added WIP new csv output format

---
+++ silico/result/format/csv.py
@@ -0,0 +1,68 @@
+from builtins import isinstance
+import csv
+import io
+
+from silico.result.format.base import Result_dumper
+import itertools
+
+
+class CSV_dumper(Result_dumper):
+    """
+    """
+        
+    def process(self, dumped_results):
+        # First, flatten our data ready for writing.
+        flat_results = []
+        
+        for dumped_result in dumped_results:
+            flat_results.append(self.flatten_results(dumped_result))
+            
+        headers = list(itertools.chain(*[list(flat_result.keys()) for flat_result in flat_results]))
+        
+        headers = list(dict.fromkeys(headers))
+        
+        
+        stream = io.StringIO()
+        writer = csv.DictWriter(stream, headers)
+        
+        writer.writeheader()
+        for flat_result in flat_results:
+            writer.writerow(flat_result)
+            
+        return stream.getvalue()
+        
+    def flatten(self, dumped_result):
+        """
+        Flatten a recursive list and/or dict so it can be printed as a single, non-nested, row.
+        """
+        flat_result = {}
+        
+        for name, item in dumped_result.items():
+            if isinstance(item, list):
+                # Don't include lists (they are too big to flatten).
+                continue
+            
+            if isinstance(item, dict):
+                if "value" in item and "units" in item:
+                    flat_result[name + " / " + item['units']] = item['value']
+                    item.pop("units")
+                    item.pop("value")
+                
+                flat_child = self.flatten(item)
+                flat_result.update({"{}_{}".format(name, child_name): value for child_name, value in flat_child.items()})
+            
+            else:
+                flat_result[name] = item
+                
+        return flat_result
+            
+    def flatten_results(self, dumped_result):
+        """
+        Flatten a recursive list and/or dict so it can be printed as a single, non-nested, row.
+        """
+        flat_result = {}
+        
+        for filtered in dumped_result:
+            flat_result.update(self.flatten(filtered))
+                
+        return flat_result
