95d50ac7c2041e109a7610a481fee6d6509e748c
oliver-s-lee <osl@st-andrews.ac.uk>
2022-07-29 09:33:00 +0100

    Added default_filters property to dumpers so control what is automatically filtered

--- silico/result/format/base.py
+++ silico/result/format/base.py
@@ -1,3 +1,4 @@
+from silico.result.format.filter import Result_filter
 
 class Result_dumper():
     """
@@ -7,9 +8,22 @@ class Result_dumper():
     def __init__(self, filters, silico_options):
         self.filters = filters
         self.silico_options = silico_options
+    
+    @property
+    def default_filters(self):
+        """
+        Return a list of filter objects that will be used by default (when none are specified explicitly).
+        """
+        # This filter simply returns all data.
+        return [Result_filter("", self.silico_options)]
         
     def dump(self, results):
         data = []
+        
+        filters = self.filters
+        if len(filters) == 0:
+            # No filter specified, use a default filter.
+            filters = self.default_filters
         
         for result in results:
             datum = []            
