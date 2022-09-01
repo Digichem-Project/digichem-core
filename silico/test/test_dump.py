6dd604703bf99cf0884937621453204d63c66be5
oliver-s-lee <osl@st-andrews.ac.uk>
2022-09-01 14:08:34 +0100

    Added initial numeric result dumping test

--- silico/test/test_dump.py
+++ silico/test/test_dump.py
@@ -1,10 +1,12 @@
 """Tests for resulting dumping (ie, output from `silico result`)."""
 
 import pytest
+import yaml
 
 from silico.test.util import silico_options
 from silico.test.test_result import gaussian_opt_result, gaussian_ES_result
 from silico.result.format.filter import Result_filter
+from silico.result.format.yaml import Yaml_dumper
 
 
 # Tests for checking the filtering mechanism is working correctly.
@@ -30,3 +32,18 @@ from silico.result.format.filter import Result_filter
 def test_filter(gaussian_opt_result, filter_string, silico_options):
     """Basic filtering tests."""
     Result_filter(filter_string, silico_options).filter(gaussian_opt_result)
+
+
+@pytest.mark.parametrize("result_set, filter_string, value", [
+        (pytest.lazy_fixture("gaussian_opt_result"), "energies:scf:final:value", -10488.990333747),
+    ])
+def test_numeric_result(result_set, filter_string, value, silico_options):
+    """Dumping of numeric values."""
+    # Dump the result to text (YAML).
+    data = Yaml_dumper.from_text(filter_string, silico_options = silico_options).dump(result_set)
+    
+    # Parse with yaml.
+    parsed = yaml.safe_load(data)
+    
+    # Check the value
+    assert list(parsed.items())[0][1] == pytest.approx(value, abs=1e-5)
