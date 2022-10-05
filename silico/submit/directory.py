83330b87458ecc9e9edff624143838b311b4c955
oliver-s-lee <osl@st-andrews.ac.uk>
2022-10-05 13:35:19 +0100

    Fixed incorrect usage of name over meta['name'] in Calculation_directory

--- silico/submit/directory.py
+++ silico/submit/directory.py
@@ -109,8 +109,8 @@ class Calculation_directory(Silico_directory):
         """
         return self(
             Molecule_directory.from_calculation(calculation),
-            calculation.program.name,
-            self.safe_name(calculation.name),
+            calculation.program.meta['name'],
+            self.safe_name(calculation.meta['name']),
             create = create,
             program_sub_folder = calculation.structure['program_sub_folder'],
             prepend_program_name = calculation.structure['prepend_program_name'],
