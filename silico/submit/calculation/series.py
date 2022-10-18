d3fee1283c65832a90a86f18e57818f45996af09
oliver-s-lee <osl@st-andrews.ac.uk>
2022-10-18 09:42:35 +0100

    Updated series calculations to new style options

--- silico/submit/calculation/series.py
+++ silico/submit/calculation/series.py
@@ -2,6 +2,7 @@ from silico.submit.calculation import Calculation_target
 from silico.config.configurable.option import Option
 from silico.exception.configurable import Configurable_exception
 from silico.submit.memory import Memory
+from silico.config.configurable.options import Options
 
 
 class Calculation_series(Calculation_target):
@@ -22,13 +23,18 @@ class Calculation_series(Calculation_target):
         no_edit = True
     )
     
-    memory = Option(help = "Force each calculation in this series to use this amount of memory", default = None, type = Memory)
-    num_cpu = Option(help = "Force each calculation in this series to use this many CPUs", default = None, type = int)
-    _combined_report_name = Option("combined_report_name", help = "The name to use for the folder in which the combined report for this series calculation will be written", default = None)
+    performance = Options(help = "Options for controlling functional aspects of the calculation.",
+        memory = Option(help = "Force each calculation in this series to use this amount of memory.", default = None, type = Memory),
+        num_cpu = Option(help = "Force each calculation in this series to use this many CPUs.", default = None, type = int)
+    )
+    post_process = Options(name = "post", help = "Options that control post processing of the calculation results.",
+        combined_report_name = Option(help = "The name to use for the folder in which the combined report for this series calculation will be written. If not specified, the name of this series will be used.", default = None)
+    )
+    
             
     @property
     def combined_report_name(self):
-        return self._combined_report_name if self._combined_report_name is not None else self.meta['name']
+        return self.post_process['combined_report_name'] if self.post_process['combined_report_name'] is not None else self.meta['name']
     
     def expand(self, calculations):
         """
@@ -49,8 +55,8 @@ class Calculation_series(Calculation_target):
                 raise Configurable_exception(self, "Could not expand to real calculation with TAG path '{}'".format(tag_path)) from e
                 
             # Overwrite the memory and CPUs if given.
-            if self.num_cpu is not None:
-                calc.num_cpu = self.num_cpu
+            if self.performance['num_cpu'] is not None:
+                calc.performance['num_cpu'] = self.performance['num_cpu']
         
             if self.performance['memory'] is not None:
                 # NOTE: memory is an object, this will set the same object reference for all sub-calcs.
