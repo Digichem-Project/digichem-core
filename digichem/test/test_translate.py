4075dc6ec17454e5a66fb6e302d18ead60785565
oliver-s-lee <osl@st-andrews.ac.uk>
2022-10-21 16:37:24 +0100

    Added initial test for basis set conversion

--- digichem/test/test_translate.py
+++ digichem/test/test_translate.py
@@ -1,11 +1,11 @@
 """Tests for translation classes."""
-from silico.submit.translate import Solvent
+from silico.submit.translate import Solvent, Basis_set
 import pytest
 
 
 def test_solvent():
     """
-    Test conversion for DCM.
+    Test conversions for DCM.
     """
     # Get some different ways to represent DCM.
     solvents = (Solvent("dcm"), Solvent("DCM"), Solvent("dichloroMethane"), Solvent("Dichloro Methane"))
@@ -18,3 +18,20 @@ def test_solvent():
     
     # And epsilon
     assert all([pytest.approx(8.93) == solvent.translate("epsilon") for solvent in solvents])
+    
+
+def test_basis_set():
+    """
+    Test conversions for basis sets.
+    """
+    # Test both types of 6-31G**.
+    basis_sets = (Basis_set("6-31g(d,p)"), Basis_set("6-31G**"))
+    
+    # Check both are equivalent.
+    assert str(basis_sets[0]) == str(basis_sets[1])
+    
+    # Check Gaussian version.
+    assert all(["6-31G**" == basis_set.to_gaussian() for basis_set in basis_sets])
+    
+    # Check Turbomole version.
+    assert all(["6-31G**" == basis_set.to_turbomole() for basis_set in basis_sets])
