236c24e4afa881005c2d53d9458485412f1212a1
oliver-s-lee <osl@st-andrews.ac.uk>
2022-10-27 16:17:09 +0100

    Added new identifier class for resolving config identifier strings

--- digichem/config/configurable/identifier.py
+++ digichem/config/configurable/identifier.py
@@ -1,55 +1,72 @@
-import shlex
+import csv
 
-# This splitting mechanism is now deprecated because it interferes with parsing of tag lists.
-#
-# Splitting with shlex strips out quote marks, normally this is a good thing but it makes it hard to
-# specify tag lists which need to be quoted,
-# eg:
-#
-# Single Node SLURM/Gaussian 16/[Opt, B3LYP, Gas-Phase, '6-31G(d,p)']
-#
-# Becomes a list that looks like:
-#
-# ["Single Node SLURM", "Gaussian 16", "[Opt, B3LYP, Gas-Phase, 6-31G(d,p)]"]
-#
-# When this gets parsed by yaml, the final tag path will then get parsed incorrectly into:
-#
-# ["Single Node SLURM", "Gaussian 16", ["Opt", "B3LYP", "Gas-Phase", "6-31G(d", "p)"]]
-#
-# The solution is to simply use split("/"), this is less intelligent but preserves whitespace etc.
-# To make this work, the '/' character is now disallowed in tag names (which probably makes sense anyway).
- 
-
-class ID_splitter(shlex.shlex):
-    """
-    A custom parser used for splitting strings which identify certain configurables.
-    
-    An identifier is something that can be used to identify a particular configurable loader.
-    
-    Typically they are either an integer index or a string describing a path to a particular loader.
-    """
-    
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, posix = True)
-        # We only have one splitting character.
-        self.whitespace = "/"
-        self.whitespace_split = True
+import silico.log
+from silico.misc.base import is_int
 
 
-class Tag_splitter(shlex.shlex):
+class Identifier():
     """
-    A custom parser used for splitting strings which identify certain configurables.
-    
-    An identifier is something that can be used to identify a particular configurable loader.
-    
-    Typically they are either an integer index or a string describing a path to a particular loader.
+    A class that identifies a configurable. This could be:
+     - A single integer (which would reference a loader index).
+     - A string (which would reference a loader tag).
+     - A list of strings (which would reference a loader path).
     """
-    # TODO: Could CSV work here?
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs, posix = True)
-        # We only have one splitting character.
-        self.whitespace = ";,"
-        self.whitespace_split = True
+    def __init__(self, identifier):
+        # Use csv to split our string.
+        parser = csv.reader([identifier], delimiter = ":")
+        
+        # First, determine if a Namespace has been defined by a double colon.
+        split = next(parser)
+        
+        # A namespace is defined by an initial double colon ('::').
+        # We can check for this by looking if the second item is empty.
+        if len(split) > 2 and split[1] == "":
+            self.namespace = split[0].strip()
+            split = split[2:]
+        
+        else:
+            self.namespace = None
+        
+        # Remove excess whitespace, add namespace (if given) and remove empty parts.
+        self.raw_tag_list = []
+        for tag_part in split:
+            # Remove surrounding whitespace.
+            # NOTE: This is nearly always the right thing to do, but currently there's no way to turn this off.
+            # This means we cannot select tags that start or end with whitespace.
+            tag_part = tag_part.strip()
+            
+            # Skip if empty.
+            if tag_part == "":
+                silico.log.get_logger().warn("ignoring blank section of method tag list")
+                continue
+            
+            # Add
+            self.raw_tag_list.append(tag_part)
+            
+        # Add a list with namespace.
+        if self.namespace is not None:
+            self.tag_list = [self.namespace + " " + tag_part for tag_part in self.raw_tag_list]
+        
+        else:
+            # NOTE: This list is the same object as raw_tag_list, where as above it is a separate list...
+            self.tag_list = self.raw_tag_list
+        
+        # Now get our actual value.
+        # Turn single numbers into int.
+        if len(self.tag_list) == 1 and is_int(self.tag_list[0]):
+            self.value = int(self.tag_list[0])
+        
+        else:
+            self.value = self.tag_list
     
+    
+    def __str__(self):
+        if not isinstance(self.value, list):
+            return str(self.value)
+        
+        elif self.namespace is not None:
+            return self.namespace + ":: " + " : ".join(self.raw_tag_list)
+        
+        else:
+            return " : ".join(self.raw_tag_list)
