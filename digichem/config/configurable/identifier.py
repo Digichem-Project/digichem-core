55c0a875e788b714d95142685891be435fd64af5
oliver-s-lee <osl@st-andrews.ac.uk>
2022-10-27 11:47:56 +0100

    WIP new method reference style

--- digichem/config/configurable/identifier.py
+++ digichem/config/configurable/identifier.py
@@ -35,4 +35,21 @@ class ID_splitter(shlex.shlex):
         # We only have one splitting character.
         self.whitespace = "/"
         self.whitespace_split = True
+
+
+class Tag_splitter(shlex.shlex):
+    """
+    A custom parser used for splitting strings which identify certain configurables.
+    
+    An identifier is something that can be used to identify a particular configurable loader.
+    
+    Typically they are either an integer index or a string describing a path to a particular loader.
+    """
+    # TODO: Could CSV work here?
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, posix = True)
+        # We only have one splitting character.
+        self.whitespace = ";,"
+        self.whitespace_split = True
     
