20f4630d582870fb2d76ab5b5c397e4486b49e76
oliver-s-lee <osl@st-andrews.ac.uk>
2023-01-11 09:16:58 +0000

    Added abs argument to test.util check_dipole()

--- digichem/test/util.py
+++ digichem/test/util.py
@@ -54,14 +54,14 @@ def check_float_list(test_list, expected_list, abs = 1e-4):
         assert test_list[list_index] == pytest.approx(expected_list[list_index], abs=abs)
 
 
-def check_dipole(dipole_moment, coords):
+def check_dipole(dipole_moment, coords, abs = 1e-4):
     """
     Helper function to check a dipole moment (PDM, TEDM or TMDM) matches some expected values.
     """
-    check_float_list(dipole_moment.vector_coords, coords)
+    check_float_list(dipole_moment.vector_coords, coords, abs)
         
     # Check total
-    assert dipole_moment.total == pytest.approx((coords[0] **2 + coords[1] **2 + coords[2] **2) **0.5, abs=1e-4)
+    assert dipole_moment.total == pytest.approx((coords[0] **2 + coords[1] **2 + coords[2] **2) **0.5, abs = abs)
 
 
 def check_orbitals(orbitals, num_occ, num_unocc, homo, lumo):
