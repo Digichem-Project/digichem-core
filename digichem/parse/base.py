df46df7840481a921aadeac338cb45fade941b92
oliver-s-lee <osl@st-andrews.ac.uk>
2023-01-12 22:14:42 +0000

    Refactored search for log files and added ability to find .sir files

--- digichem/parse/base.py
+++ digichem/parse/base.py
@@ -72,57 +72,61 @@ class Parser_abc():
         """
         hint = Path(hint)
         
-        attempt = 1
-        while attempt < 2:
-            attempt += 1
-            # First, find our parent dir.
-            # hint may actually be a dir.
-            if hint.is_dir():
-                # Look for all .log files.
-                # File extensions that we recognise.
-                log_types = ["*.log", "*.out", "*.sir"]
-                parent = hint
-                log_files = [found_log_file for found_log_file in itertools.chain(*[parent.glob(log_type) for log_type in log_types])]
-                
-                #log_files = [Path(found_log_file) for found_log_file in iglob(str(Path(parent, "*.log")))]
-                # Remove any 'silico.log' files as we know these are not calc log files.
-                # We don't actually write 'silico.log' files anymore either (we use silico.out instead),
-                # but older versions did...
-                log_files = [log_file for log_file in log_files if log_file.name not in ["silico.log", "silico.out"]]
+        # First, find our parent dir.
+        # hint may actually be a dir.
+        if hint.is_dir():
+            # Look for all .log files.
+            # File extensions that we recognise.
+            log_types = ["*.log", "*.out", "*.sir"]
+            parent = hint
+            log_files = [found_log_file for found_log_file in itertools.chain(*[parent.glob(log_type) for log_type in log_types])]
+            
+            #log_files = [Path(found_log_file) for found_log_file in iglob(str(Path(parent, "*.log")))]
+            # Remove any 'silico.log' files as we know these are not calc log files.
+            # We don't actually write 'silico.log' files anymore either (we use silico.out instead),
+            # but older versions did...
+            log_files = [log_file for log_file in log_files if log_file.name not in ["silico.log", "silico.out"]]
+        else:
+            parent = hint.parent
+            log_files = [hint]
+                            
+        # Try and find job files.
+        # These files have names like 'job.0', 'job.1' etc, ending in 'job.last'.
+        for number in itertools.count():
+            # Get the theoretical file name.
+            job_file_path = Path(parent, "job.{}".format(number))
+            
+            # See if it exists (and isn't the log_file given to us).
+            if job_file_path.exists():
+                # Add to list.
+                log_files.append(job_file_path)
             else:
-                parent = hint.parent
-                log_files = [hint]
-                                
-            # Try and find job files.
-            # These files have names like 'job.0', 'job.1' etc, ending in 'job.last'.
-            for number in itertools.count():
-                # Get the theoretical file name.
-                job_file_path = Path(parent, "job.{}".format(number))
-                
-                # See if it exists (and isn't the log_file given to us).
-                if job_file_path.exists():
-                    # Add to list.
-                    log_files.append(job_file_path)
-                else:
-                    # We've found all the numbered files.
-                    break
-                        
-            # Look for other files.
-            for maybe_file_name in ("basis", "control", "mos", "alpha", "beta", "coord", "gradient", "aoforce", "job.last", "numforce/aoforce.out"):
-                maybe_file_path = Path(parent, maybe_file_name)
-                
-                if maybe_file_path.exists():
-                    # Found it.
-                    log_files.append(maybe_file_path)
+                # We've found all the numbered files.
+                break
                     
-            # If we have no log files, and there's a directory called Output that we can use, try again using that as the hint.
-            if len(log_files) == 0 and hint.is_dir() and Path(hint, "Output").is_dir():
-                hint = Path(hint, "Output")
-                attempt -= 1
+        # Look for other files.
+        for maybe_file_name in ("basis", "control", "mos", "alpha", "beta", "coord", "gradient", "aoforce", "job.last", "numforce/aoforce.out"):
+            maybe_file_path = Path(parent, maybe_file_name)
+            
+            if maybe_file_path.exists():
+                # Found it.
+                log_files.append(maybe_file_path)
                 
         # Make sure we only have unique log files.
         # We also now reverse our ordering, so that files given earlier by the user have priority.
-        log_files = list(reversed(list(dict.fromkeys(log_files))))
+        if len(log_files) > 0:
+            log_files = list(reversed(list(dict.fromkeys(log_files))))
+            return log_files
+                
+        # If we have no log files, and there's a directory called Output or Result that we can use, try again using that as the hint.
+        elif hint.is_dir():
+            if Path(hint, "Results").is_dir():
+                log_files = self.find_log_files(Path(hint, "Results"))
+            
+            # If we still have nothing, try the output dir.
+            if len(log_files) == 0 and Path(hint, "Output").is_dir():
+                log_files = self.find_log_files(Path(hint, "Output"))
+                
         return log_files
     
     @classmethod
