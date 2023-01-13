dc6fca4e70af036ecb780b60d58549ae011521d5
oliver-s-lee <osl@st-andrews.ac.uk>
2023-01-13 16:53:24 +0000

    Removed unused dumpable() class

--- digichem/result/result.py
+++ digichem/result/result.py
@@ -15,15 +15,6 @@ import silico.result.excited_state
 from silico.result.emission import Emissions
 
 
-class Dumpable():
-    """
-    Class for returning results on request that can still be dumped.
-    """
-
-    def dump(self, silico_options):
-        raise NotImplementedError("Implement in subclass")
-
-
 class Result_set(Result_object):
     """
     Class that represents a collection of results from a calculation.
