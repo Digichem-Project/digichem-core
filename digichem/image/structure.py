89ee7d7ace61f917f766025abc89b9f326a1eb01
oliver-s-lee <osl@st-andrews.ac.uk>
2023-03-30 20:20:59 +0100

    Expanded 2D structure generation to allow specifying output resolution relative to the molecule size, added options for showing explicit H and for atom numbering

--- digichem/image/structure.py
+++ digichem/image/structure.py
@@ -6,6 +6,8 @@ import silico.log
 
 # Silico imports.
 from silico.image.base import Image_maker
+from silico.result.atom import Atom_list
+from silico.result.alignment.base import Minimal
 
 
 class Skeletal_image_maker(Image_maker):
@@ -13,7 +15,7 @@ class Skeletal_image_maker(Image_maker):
     A class for rendering skeletal-style molecule structure images.
     """
     
-    def __init__(self, output, coords, *args, resolution = 1024, render_backend = "rdkit", **kwargs):
+    def __init__(self, output, coords, *args, abs_resolution = None, rel_resolution = 100, render_backend = "rdkit", numbering = False, explicit_h = False, **kwargs):
         """
         Constructor for Structure_image_maker objects.
         
@@ -21,10 +23,15 @@ class Skeletal_image_maker(Image_maker):
         :param coords: The coordinates of the molecule/system to render as a Silico_coords object.
         :param resolution: The width and height of the rendered image.
         :param render_backend: The library to prefer to use for rendering, possible options are 'rdkit' or 'obabel'. If 'rdkit' is chosen but is not available, obabel will be used as a fallback. 
+        :param numbering: Whether to show atom numberings.
+        :param explicit_h: Whether to show explicit H.
         """
         self.coords = coords
-        self.resolution = resolution
+        self.abs_resolution = abs_resolution
+        self.rel_resolution = rel_resolution
         self.render_backend = render_backend
+        self.numbering = numbering
+        self.explicit_h = explicit_h
         assert self.render_backend == "rdkit" or self.render_backend == "obabel"
         super().__init__(output, *args, **kwargs)
         
@@ -36,7 +43,8 @@ class Skeletal_image_maker(Image_maker):
         return self(
             output,
             coords = coords,
-            resolution = options['skeletal_image']['resolution'],
+            abs_resolution = options['skeletal_image']['resolution']['absolute'],
+            rel_resolution = options['skeletal_image']['resolution']['relative'],
             render_backend = options['skeletal_image']['render_backend'],
             **kwargs
         )
@@ -45,6 +53,12 @@ class Skeletal_image_maker(Image_maker):
         """
         Make the image described by this object.
         """
+        if self.abs_resolution:
+            resolution = self.abs_resolution
+        
+        else:
+            resolution = int(self.rel_resolution * Minimal.from_coords(self.coords).X_length)
+        
         render_backend = self.render_backend
         if render_backend == "rdkit":
             # Try and import rdkit.
@@ -76,19 +90,21 @@ class Skeletal_image_maker(Image_maker):
             # for now, disable logging...
             rdkit.RDLogger.DisableLog('rdApp.*')
             
-            molecule = rdkit.Chem.MolFromMolBlock(self.coords.to_format("mol"))
+            molecule = rdkit.Chem.MolFromMolBlock(self.coords.to_format("mol"), removeHs = not self.explicit_h)
             # rdkit will silently return None if parsing fails, best to a check.
             if molecule is None:
                 raise Exception("Failed to parse coordinates with rdkit")
+            
             rdkit.Chem.AllChem.Compute2DCoords(molecule)
             
-            # Add atom labelling.
-            for atom in molecule.GetAtoms():
-                # For each atom, set the property "atomNote" to a index+1 of the atom
-                molecule.SetProp("atomNote", str(atom.GetIdx()+1))
+            if self.numbering:
+                # Add atom labelling.
+                for atom in molecule.GetAtoms():
+                    #atom.SetProp("molAtomMapNumber", str(atom.GetIdx()+1))
+                    atom.SetProp("atomNote", str(atom.GetIdx()+1))
             
             # Then write the file.
-            rdkit.Chem.Draw.MolToFile(molecule, str(self.output), (self.resolution, self.resolution))
+            rdkit.Chem.Draw.MolToFile(molecule, str(self.output), (resolution, resolution))
             
         else:
             # We've been asked to use obabel
