c4fb874dd5c96c569646232ad7600df7ee35ee09
oliver-s-lee <osl@st-andrews.ac.uk>
2023-04-13 12:58:59 +0100

    Added powerset function (from python itertool recipes) to misc

--- digichem/misc/base.py
+++ digichem/misc/base.py
@@ -1,3 +1,10 @@
+from itertools import chain, combinations
+
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
 def regular_range(median, number, spacing):
     """
     Generate a range of numbers with a given median and spacing.
