5a0532285a0693fe95463abb10883aa7b6014e70
oliver-s-lee <osl@st-andrews.ac.uk>
2023-05-30 14:39:09 +0100

    Added concept of 'history' to calculations, to show which calcs derive from others

--- digichem/input/base.py
+++ digichem/input/base.py
@@ -8,7 +8,7 @@ class Input_file():
     In most cases the concrete class silico.input.Input_coords is what you are likely looking for.
     """
     
-    def __init__(self, charge = None, multiplicity = None, name = None, file_name = None):
+    def __init__(self, charge = None, multiplicity = None, name = None, file_name = None, history = None):
         """
         Abstract constructor for Input_file classes.
         
@@ -16,6 +16,7 @@ class Input_file():
         :param multiplicity: An explicit molecular multiplicity (as an integer). If not given, a multiplicity may be interpreted from other properties of the input file.
         :param name: An explicit molecule name. If not given, a name may be interpreted from other properties of the input file.
         :param file_name: The path to the file from which this object was created. If this Input_file was not created from a file (eg, from memory instead), this should be None.
+        :param history: The SHA of the previous calculation (if applicable) from which these coordinates were taken.
         """
         if charge is not None and not isinstance(charge, int):
             raise TypeError("Charge must be an integer (or None)")
@@ -25,8 +26,9 @@ class Input_file():
         self.charge = charge
         self.multiplicity = multiplicity
         self.name = name
-        # TOOD: This shoud be called file_path
+        # TOOD: This should be called file_path
         self.file_name = Path(file_name) if file_name is not None else None
+        self.history = history
     
     @property
     def implicit_name(self):
