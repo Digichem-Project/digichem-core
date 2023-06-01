d28ace45a82bee1da1586e943879d87de674dd14
oliver-s-lee <osl@st-andrews.ac.uk>
2023-06-01 08:54:00 +0100

    Added ability to run some tests in parallel with xdist

--- digichem/test/conftest.py
+++ digichem/test/conftest.py
@@ -6,7 +6,11 @@ numpy.seterr(invalid = 'raise', divide = 'raise')
 
 def pytest_addoption(parser):
     parser.addoption(
-        "--run-slow", action="store_true", default=False, help="run slow tests"
+        "--run-slow", action="store_true", default=False, help="run slow tests")
+    parser.addoption(
+        "--run-parallel-only", action="store_true", default=False, help="only run tests that can be ran in parallel")
+    parser.addoption(
+        "--run-series-only", action="store_true", default=False, help="only run tests that can be ran in series"
     )
 
 
@@ -15,11 +19,14 @@ def pytest_configure(config):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--run-slow"):
-        # Asked to run slow tests, do nothing.
-        return
+#     if config.getoption("--run-slow"):
+#         # Asked to run slow tests, do nothing.
+#         return
     
+
     skip_submit = pytest.mark.skip(reason="need --run-slow option to run")
     for item in items:
-        if "slow" in item.keywords:
+        if     ("slow" in item.keywords and not config.getoption("--run-slow") and not config.getoption("--run-series-only")) \
+            or ("slow" in item.keywords and config.getoption("--run-parallel-only")) \
+            or ("slow" not in item.keywords and config.getoption("--run-series-only")):
             item.add_marker(skip_submit)
