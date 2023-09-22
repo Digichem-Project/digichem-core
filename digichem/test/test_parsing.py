c2f5ac6e412ce90ac9aa94afafb2b21d2f131cd9
oliver-s-lee <osl@st-andrews.ac.uk>
2023-09-22 07:42:40 +0100

    Expanded dump tests to include merged result

--- digichem/test/test_parsing.py
+++ digichem/test/test_parsing.py
@@ -36,14 +36,27 @@ def test_merged_parsing(data_set, silico_options):
     assert len(result.results) == len(data_set)
 
 
-@pytest.mark.parametrize("result_file", [result_file for program_files in result_files.values() for result_file in program_files])
-def test_dump_and_parse(result_file, tmp_path, silico_options):
+@pytest.mark.parametrize(
+    "result_files",
+    itertools.chain(
+        [(result_file,) for program_files in result_files.values() for result_file in program_files],
+        [(
+            Path(data_directory(), "Naphthalene/Gaussian 16 Optimisation Frequencies PBE1PBE (GD3BJ) Toluene 6-31G(d,p)"),
+            Path(data_directory(), "Naphthalene/Gaussian 16 Excited States TDA Optimised S(1) PBE1PBE (GD3BJ) Toluene 6-31G(d,p).tar.gz")
+        )]
+    )
+)
+def test_dump_and_parse(result_files, tmp_path, silico_options):
     """Test whether we can dump and reload a result to get the same data back again."""
     # Set numpy errors (not sure why this isn't the default...)
     numpy.seterr(invalid = 'raise', divide = 'raise')
     
     # Parse the raw data.
-    raw = parse_calculation(result_file, options = silico_options)
+    if len(result_files) == 1:
+        raw = parse_calculation(result_files[0], options = silico_options)
+    
+    else:
+        raw = parse_and_merge_calculations(*result_files, options = silico_options)
     
     # Instead of just dumping to memory we'll dump to a file to test the full mechanism.
     with open(Path(tmp_path, "dump.sir"), "wt") as dump_file:
