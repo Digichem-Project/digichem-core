43a0beb82d6b75bb16261556dd0ce2e90c10573a
oliver-s-lee <osl@st-andrews.ac.uk>
2023-11-25 12:45:20 +0000

    Fixed default log-level not being reset after loading config options

--- digichem/config/__init__.py
+++ digichem/config/__init__.py
@@ -28,39 +28,41 @@ def get_config(extra_config_files = None, extra_config_strings = None):
         log_level = get_logger().level
         get_logger().setLevel("DEBUG")
         
-        # First, load options if not already done so.
-        if options is None:
-            # Load config options from file.
-            # These objects are simple dicts.
-            config = Config_file_parser(master_config_path).load(True)
-            config.merge(Config_file_parser(system_config_location).load(True))
-            config.merge(Config_file_parser(user_config_location).load(True))
+        try:
+            # First, load options if not already done so.
+            if options is None:
+                # Load config options from file.
+                # These objects are simple dicts.
+                config = Config_file_parser(master_config_path).load(True)
+                config.merge(Config_file_parser(system_config_location).load(True))
+                config.merge(Config_file_parser(user_config_location).load(True))
+                
+                # No need to validate here, we're going to do it later anyway.
+                globals()['options'] = Silico_options(validate_now = False, **config)
             
-            # No need to validate here, we're going to do it later anyway.
-            globals()['options'] = Silico_options(validate_now = False, **config)
-        
-        if extra_config_files is None:
-            extra_config_files = []
+            if extra_config_files is None:
+                extra_config_files = []
+                
+            if extra_config_strings is None:
+                extra_config_strings = []
+                
+    #         if len(extra_config_files) == 0 and len(extra_config_strings) == 0:
+    #             # Do nothing.
+    #             return options
             
-        if extra_config_strings is None:
-            extra_config_strings = []
+            # Load any additional config files.
+            for extra_config_file in extra_config_files:
+                options.deep_merge(Config_file_parser(extra_config_file).load())
+                
+            # Then load any additional config strings.
+            for extra_config_string in extra_config_strings:
+                options.deep_merge(Config_parser(extra_config_string).load())
             
-#         if len(extra_config_files) == 0 and len(extra_config_strings) == 0:
-#             # Do nothing.
-#             return options
-        
-        # Load any additional config files.
-        for extra_config_file in extra_config_files:
-            options.deep_merge(Config_file_parser(extra_config_file).load())
+            # Check everything is valid.
+            options.validate()
             
-        # Then load any additional config strings.
-        for extra_config_string in extra_config_strings:
-            options.deep_merge(Config_parser(extra_config_string).load())
+            # And return.
+            return options
         
-        # Check everything is valid.
-        options.validate()
-        
-        # And return.
-        return options
-    
-        get_logger().setLevel(log_level)
+        finally:
+            get_logger().setLevel(log_level)
