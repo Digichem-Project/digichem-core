90af69b7f8b1232f6f6a4022166d5e260dfbaf4c
oliver-s-lee <osl@st-andrews.ac.uk>
2023-11-25 15:59:36 +0000

    Removed pkg_resources as a dependency (#65)

--- digichem/config/locations.py
+++ digichem/config/locations.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-import pkg_resources
+
 import silico
+from silico.datas import get_resource
 
 
 # A config file included in the silico install directory.
-master_config_path = Path(pkg_resources.resource_filename('silico', "data/config/silico.yaml"))
+master_config_path = get_resource("data/config/silico.yaml")
 
 # The location of the system-wide config file (which has precedence over the master).
 system_config_location = Path("/etc/silico{}/silico.yaml".format(silico.major_version))
