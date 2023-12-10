4c1e0914973c4f27668ca80a3dfea4281aff228c
oliver-s-lee <osl@st-andrews.ac.uk>
2023-12-10 22:12:01 +0000

    Bumped pre-release

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -27,7 +27,7 @@ import silico.logging
 major_version = 5
 minor_version = 2
 revision = 0
-prerelease = 4
+prerelease = 5
 # Whether this is a development version.
 development = prerelease is not None
 demonstration = False
