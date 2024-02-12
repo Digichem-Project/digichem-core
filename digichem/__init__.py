ce5cd19da9c05b15b905ef4d4f8f6fb55d7bcc5a
oliver-s-lee <osl@st-andrews.ac.uk>
2024-02-12 07:49:00 +0000

    Bumped version info

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -25,9 +25,9 @@ import silico.logging
 
 # Version information.
 major_version = 5
-minor_version = 2
-revision = 1
-prerelease = 0
+minor_version = 3
+revision = 0
+prerelease = 1
 # Whether this is a development version.
 development = prerelease is not None
 demonstration = False
