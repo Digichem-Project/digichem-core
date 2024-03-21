ebbce567700e0aa3e3ad825638f332727931e893
oliver-s-lee <osl@st-andrews.ac.uk>
2024-03-21 13:23:45 +0000

    Added new test for oprattle

--- digichem/test/test_prattle.py
+++ digichem/test/test_prattle.py
@@ -2,7 +2,9 @@
 
 import pytest
 
-from digichem.file.prattle import Oprattle_formats
+from digichem.file.prattle import Oprattle_formats, Openprattle_converter
+
+from digichem.test.util import pyridine_cml, benzene_cdx
 
 @pytest.mark.parametrize("readwrite", ["read", "write"])
 def test_formats(readwrite):
@@ -11,3 +13,24 @@ def test_formats(readwrite):
     
     else:
         assert len(Oprattle_formats().write()) > 0
+
+@pytest.mark.parametrize("file_path", [
+    pyridine_cml,
+    benzene_cdx
+])
+def test_convert(file_path):
+    Openprattle_converter(input_file_path = file_path).convert("com")
+
+
+@pytest.mark.parametrize("file_path", [
+    pyridine_cml,
+    benzene_cdx
+])
+def test_gen3d(file_path):
+    first = Openprattle_converter(input_file_path = file_path).convert("xyz", gen3D = False)
+
+    same = Openprattle_converter(input_file_path = file_path).convert("xyz", gen3D = False)
+    different = Openprattle_converter(input_file_path = file_path).convert("xyz", gen3D = True)
+
+    assert first == same
+    assert first != different
