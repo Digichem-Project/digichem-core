aa7ef008292c06e8c11c2aef7b3a5ad72a24fdc8
oliver-s-lee <osl@st-andrews.ac.uk>
2024-08-05 11:11:49 +0100

    feat: renamed new alignment methods to more descriptive names

--- digichem/parse/__init__.py
+++ digichem/parse/__init__.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore", "Module .* is under testing", category = UserW
 warnings.filterwarnings("ignore", "Module .* is not fully tested", category = UserWarning)
 
 # These alignment classes are needed to parse correctly.
-from digichem.result.alignment.brute import Brute_force, Descending_grid, Bayesian_grid
+from digichem.result.alignment.grid import Grid_search, Descending_grid, Bayesian_grid
 from digichem.result.alignment.AAA import Adjusted_average_angle
 from digichem.result.alignment.AA import Average_angle
 from digichem.result.alignment.FAP import Furthest_atom_pair
