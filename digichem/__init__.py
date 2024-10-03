c82063304861116110ca5712aca60803b93788c5
Oliver Lee <osl@st-andrews.ac.uk>
2024-10-03 09:47:03 +0000

    chore: update last modified

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -39,7 +39,7 @@ __author__ = [
 ]
 
 # Program date (when we were last updated). This is changed automatically.
-_last_updated_string = "21/08/2024"
+_last_updated_string = "03/10/2024"
 last_updated = datetime.strptime(_last_updated_string, "%d/%m/%Y")
 
 # The sys attribute 'frozen' is our flag, '_MEIPASS' is the dir location.
