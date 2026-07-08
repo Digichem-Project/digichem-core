da981273065c9360ac0344b2f318d070ec661b3e
Oliver <osl@st-andrews.ac.uk>
2026-07-08 17:29:04 +0100

    chore: bumped version to pre.4

--- digichem/__init__.py
+++ digichem/__init__.py
@@ -10,7 +10,7 @@ from digichem.datas import get_resource
 # Package metadata.#
 ####################
 
-__version__ = "8.0.0-pre.3"
+__version__ = "8.0.0-pre.4"
 _v_parts = __version__.split("-")[0].split(".")
 major_version = int(_v_parts[0])
 minor_version = int(_v_parts[1])
